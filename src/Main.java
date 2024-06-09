b55c98102c11e5a2bd47e7bdf9e2d11c4f072e06
m3mpm <m3mpm@mymail.ru>
2024-06-09 17:32:44 +0700

    add selectionSort

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {0,10,9,8,2,3};
+        int[] array = {0,10,9,8,2,3,-1};
         Sort sort = new Sort();
         sort.printArray(array);
-        sort.bubbleSort(array);
+        sort.selectionSort(array);
         sort.printArray(array);
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -1,12 +1,26 @@
 public class Sort {
     public void bubbleSort(int[] array){
         int size = array.length;
-        for (int out = size -1; out > 1; out--){
+        for (int out = size - 1; out > 1; out--){
             for (int in = 0; in < out; in++){
                 if(array[in] > array[in + 1]){
                     swap(array,in,in+1);
                 }
             }
+        }
+    }
+
+    public void selectionSort(int[] array){
+        int size = array.length;
+        int min;
+        for (int out = 0; out < size - 1; out++) {
+            min = out;
+            for (int in = out + 1; in < size ; in++) {
+                if(array[in] < array[min]){
+                    min = in;
+                }
+            }
+            swap(array,out,min);
         }
     }
 
