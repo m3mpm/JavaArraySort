ed051d804a87e4b315db72ac6e2132e55b7d0c4d
Ruslan <114345926+m3mpm@users.noreply.github.com>
2024-06-19 13:35:02 +0700

    add comments

--- src/Sort.java
+++ src/Sort.java
@@ -2,7 +2,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Sort {
 
-
+    /**
+     * Пузырьковая сортировка
+     */
     public void bubbleSort(int[] array){
         int size = array.length;
         for (int out = size - 1; out > 1; out--){
@@ -14,6 +16,9 @@ public class Sort {
         }
     }
 
+    /**
+     * Сортировка методом выбора
+     */
     public void selectionSort(int[] array){
         int size = array.length;
         int min;
@@ -28,6 +33,10 @@ public class Sort {
         }
     }
 
+
+    /**
+     * Сортировка методом вставки
+     */
     public void insertionSort(int[] array){
         int size = array.length;
         for (int out = 1; out < size; out++) {
@@ -40,6 +49,10 @@ public class Sort {
         }
     }
 
+
+    /**
+     * Перестановка элементов
+     */
     private void swap(int[] array, int one, int two){
         int tmp = array[one];
         array[one] = array[two];
